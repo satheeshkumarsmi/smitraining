0872534f4a666204cdf0089ca4973922756d9250
sys-user <sys-user@vgs059>
2019-11-28 19:30:46 +0530

    Commit From Iswarya

--- smitraining-1/src/main/java/com/smi/innothink/Smitraining1Application.java
+++ smitraining-1/src/main/java/com/smi/innothink/Smitraining1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Smitraining1Application {
 
 	public static void main(String[] args) {
+		System.out.println("welcome");
 		SpringApplication.run(Smitraining1Application.class, args);
 	}
 
