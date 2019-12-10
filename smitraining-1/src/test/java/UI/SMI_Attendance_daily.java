61d443c6a9a25a84980d8588965311492f294cbb
shanmathi <shanmathisudhakaran20497@gmail.com>
2019-12-10 17:20:40 +0530

    UI testing for attendance and batch partially for students

---
+++ smitraining-1/src/test/java/UI/SMI_Attendance_daily.java
@@ -0,0 +1,32 @@
+package UI;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SMI_Attendance_daily {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "/home/sys-user/Downloads/chromedriver");
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		String url = "https://smi-training-2020.herokuapp.com/dashboard";
+
+		driver.get(url);
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.findElement(By.className("sp")).click();
+		
+
+	}
+}
